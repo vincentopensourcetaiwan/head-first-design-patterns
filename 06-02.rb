87f0a0e515616a9c984a4abd7daccd1f5fefd686
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2016-09-05 11:05:08 +0800

    Implement RemoteControl

--- 06-02.rb
+++ 06-02.rb
@@ -27,6 +27,18 @@ class LightOnCommand < Command
   end
 end
 
+class LightOffCommand < Command
+  attr_reader :light
+
+  def initialize(light)
+    @light = light
+  end
+
+  def execute
+    @light.off
+  end
+end
+
 class SimpleRemoteControl
   attr_reader :slot
 
@@ -48,9 +60,28 @@ class RemoteControl
     @off_commands = Array.new
   end
 
+  def set_command(on_command, off_command)
+    @on_commands << on_command
+    @off_commands << off_command
+  end
+
+  def on_button_was_pushed(index)
+    @on_commands[index].execute
+  end
+
+  def off_button_was_pushed(index)
+    @off_commands[index].execute
+  end
+
 end
 
 light = Light.new
 light_on = LightOnCommand.new(light)
-remote = SimpleRemoteControl.new(light_on)
-remote.button_was_pressed
+light_off = LightOffCommand.new(light)
+
+
+remote = RemoteControl.new
+remote.set_command(light_on, light_off)
+remote.on_button_was_pushed(0)
+remote.off_button_was_pushed(0)
+
